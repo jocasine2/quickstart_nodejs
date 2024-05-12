e139d333795ee62e92e70587a38e8153fd57079d
Joaquim <38539884+jocasine2@users.noreply.github.com>
2024-05-11 23:49:05 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+// Middleware para fazer o parse do corpo das requisições JSON
+app.use(express.json());
+
 // Use os controladores para registrar as rotas
 app.use('/personagens', require('./controllers/personagem_controller'));
 
